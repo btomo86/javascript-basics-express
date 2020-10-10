31b6a6bce967e00a1427b5a1f1a1b6fa61f323bc
Ben Thompson <btomo86@gmail.com>
2020-10-10 15:14:42 +0100

    numbers completed

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const {
   sayHello,
   uppercase,
@@ -12,24 +14,6 @@ const {
 
 const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
-app.get('/numbers/add/:num1/and/:num2', (req, res) => {
-  const num1 = parseInt(req.params.num1);
-  const num2 = parseInt(req.params.num2);
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: add(num1, num2) });
-  }
-});
-
-app.get('/numbers/subtract/:num1/from/:num2', (req,res) => {
-  const num1 = parseInt(req.params.num1);
-  const num2 = parseInt(req.params.num2);
-  if (isNaN(num1) || isNaN(num2)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: subtract(num1, num2) });
-  }
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200);
   res.json({ result: sayHello(req.params.string) });
@@ -52,5 +36,79 @@ app.get('/strings/first-characters/:string', (req, res) => {
     res.status(200).json({ result: firstCharacters(req.params.string, req.query.length) });
   }
 });
+
+app.get('/numbers/add/:num1/and/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: add(num1, num2) });
+  }
+});
+app.get('/numbers/subtract/:num1/from/:num2', (req, res) => {
+  const num1 = parseInt(req.params.num2);
+  const num2 = parseInt(req.params.num1);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: subtract(num1, num2) });
+  }
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: multiply(a, b) });
+  }
+});
+
+app.post('/numbers/divide', (req, res) => {
+  if (req.body.a === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (req.body.b === 0) {
+    res.status(400).json({ error: 'not divisable by 0.' });
+  } else if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: divide(a, b) });
+  }
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  if (req.body.a === 0) {
+    res.status(200).json({ result: 0 });
+  } else if (req.body.b === 0) {
+    res.status(400).json({ error: 'not divisable by 0.' });
+  } else if (!req.body.a || !req.body.b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
+
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).send({ result: remainder(a, b) });
+  }
+
 
 module.exports = app;
